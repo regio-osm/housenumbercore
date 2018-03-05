f3cf59070795ebab71d642493c0bd6fd39be8114
okilimu <githubb@diesei.de>
2018-03-05 23:43:42 +0100

    modified .travis.yml file for use java compiler version at least 7 add junit case for class CsvReaderTest, but still without active cases

---
+++ test/de/regioosm/housenumbercore/imports/CsvReaderTest.java
@@ -0,0 +1,59 @@
+package de.regioosm.housenumbercore.imports;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CsvReaderTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCsvReader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetHousenumberFieldseparators() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFieldSeparator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetHeaderfield() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNext() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitialiseLuxembourg() {
+		fail("Not yet implemented");
+	}
+
+}
