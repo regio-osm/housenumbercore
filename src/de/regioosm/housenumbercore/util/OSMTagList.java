3196bf48fbba5a45a2b2bf98e65d1b8b88792226
okilimu <githubb@diesei.de>
2019-10-22 21:31:19 +0200

    add support for custom fields in csv file. For example, use ref-column content, postal code etc.

--- src/de/regioosm/housenumbercore/util/OSMTagList.java
+++ src/de/regioosm/housenumbercore/util/OSMTagList.java
@@ -26,8 +26,45 @@ public class OSMTagList {
 	public List<OSMTag> getTags() {
 		return tags;
 	}
+
+	public OSMTag getTag( String searchkey ) {
+		for( int listindex = 0; listindex < tags.size(); listindex++ ) {
+			if ( tags.get(listindex).equals(searchkey) )
+				return tags.get(listindex);
+		}
+		return null;
+	}
+
+	public String getKey( int index ) {
+		if ( index < tags.size() )
+			return tags.get(index).getKey();
+		return null;
+	}
+
+	public String getValue( int index ) {
+		if ( index < tags.size() )
+			return tags.get(index).getValue();
+		return null;
+	}
+
+	public String getValue( String searchkey ) {
+		for( int listindex = 0; listindex < tags.size(); listindex++ ) {
+			if ( tags.get(listindex).equals(searchkey) )
+				return tags.get(listindex).getValue();
+		}
+		return null;
+	}
+
+	public boolean hasKey( String searchkey ) {
+		for( int listindex = 0; listindex < tags.size(); listindex++ ) {
+			if ( tags.get(listindex).equals(searchkey) )
+				return true;
+		}
+		return false;
+	}
 	
 	public int size() {
 		return tags.size();
 	}
+
 }
